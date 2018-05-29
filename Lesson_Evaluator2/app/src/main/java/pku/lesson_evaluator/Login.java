f96d90aeb9cd0e63c8a5e47e3eac7171b0f85067
afawa <770150694@qq.com>
2018-05-29 20:23:02 +0800

    gui changes

--- Lesson_Evaluator2/app/src/main/java/pku/lesson_evaluator/Login.java
+++ Lesson_Evaluator2/app/src/main/java/pku/lesson_evaluator/Login.java
@@ -2,6 +2,7 @@ package pku.lesson_evaluator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        View view=View.inflate(this,R.layout.toolbar,null);
+        android.support.v7.widget.Toolbar toolbar=view.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
--- Lesson_Evaluator2/app/src/main/java/pku/lesson_evaluator/SignIn.java
+++ Lesson_Evaluator2/app/src/main/java/pku/lesson_evaluator/SignIn.java
@@ -2,6 +2,7 @@ package pku.lesson_evaluator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignIn extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        View view=View.inflate(this,R.layout.toolbar,null);
+        android.support.v7.widget.Toolbar toolbar=view.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
